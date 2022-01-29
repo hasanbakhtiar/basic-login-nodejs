569f8c086dd8df3d0d3b46a0e9dd4b7d85bf90f5
Hasan Bakhtiar <52384595+HasanBakhtiar@users.noreply.github.com>
2022-01-30 02:01:56 +0400

    login

--- server.js
+++ server.js
@@ -13,10 +13,7 @@ app.get('/users',(req,res)=>{
 
 app.post('/users', async (req,res)=>{
     try{
-        const salt = await bcrypt.genSalt();
-        const hashedPassword = await bcrypt.hash(req.body.password,salt);
-        console.log(salt);
-        console.log(hashedPassword);
+        const hashedPassword = await bcrypt.hash(req.body.password,10);
         const user = {name:req.body.name, password: hashedPassword}
         users.push(user)
         res.status(201).send()
@@ -24,4 +21,20 @@ app.post('/users', async (req,res)=>{
         res.status(500).send();
     }
 })
+
+app.post('/users/login', async (req,res)=>{
+    const user = users.find(user=>user.name = req.body.name)
+    if (user == null) {
+        return res.status(400).send("Cannot find user")
+    }try{
+       if(await bcrypt.compare(req.body.password, user.password)){
+           res.send("Success");
+       }else{
+           res.send('Not Allowed')
+       }
+    }catch{
+        res.status(500).send()
+    }
+})
+
 app.listen(3000);

986f4870864d6bb113215b001685c8ea712b7576
Hasan Bakhtiar <52384595+HasanBakhtiar@users.noreply.github.com>
2022-01-30 02:07:47 +0400

    login

--- server.js
+++ server.js
@@ -26,6 +26,14 @@ app.post('/users/login', async (req,res)=>{
     const user = users.find(user=>user.name = req.body.name)
     if (user == null) {
         return res.status(400).send("Cannot find user")
+    }try{
+       if(await bcrypt.compare(req.body.password, user.password)){
+           res.send("Success");
+       }else{
+           res.send('Not Allowed')
+       }
+    }catch{
+        res.status(500).send()
     }
 })
 
